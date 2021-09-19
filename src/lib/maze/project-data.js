0d3ac5010b8288ab803f4bf1b6ff2ebc9d029d71
Rodrigo Varela <rodvar@gmail.com>
2021-09-19 15:56:11 +1000

    loading maze as default project

---
+++ src/lib/maze/project-data.js
@@ -0,0 +1,337 @@
+/**
+ * Generate a localized version of the default project
+ * @param {function} translateFunction a function to use for translating the default names
+ * @return {object} the project data json for the default project
+ */
+const projectData = translateFunction => {
+    return ({
+        "targets": [{
+            "isStage": true,
+            "name": "Stage",
+            "variables": {
+                "`jEk@4|i[#Fk?(8x)AV.-my variable": ["my variable", 0]
+            },
+            "lists": {},
+            "broadcasts": {
+                "0bXua}n4vCATlxO^M2Ml": "Done"
+            },
+            "blocks": {
+                "pm3:w3q@Aiot+(:kKlfW": {
+                    "opcode": "event_whenflagclicked",
+                    "next": "@6Cl|hM^!?g,gyNLCZKb",
+                    "parent": null,
+                    "inputs": {},
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": true,
+                    "x": 435,
+                    "y": 53
+                },
+                "@6Cl|hM^!?g,gyNLCZKb": {
+                    "opcode": "looks_switchbackdropto",
+                    "next": null,
+                    "parent": "pm3:w3q@Aiot+(:kKlfW",
+                    "inputs": {
+                        "BACKDROP": [1, "W2EY9BT4j,yly[O?)L:{"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "W2EY9BT4j,yly[O?)L:{": {
+                    "opcode": "looks_backdrops",
+                    "next": null,
+                    "parent": "@6Cl|hM^!?g,gyNLCZKb",
+                    "inputs": {},
+                    "fields": {
+                        "BACKDROP": ["back", null]
+                    },
+                    "shadow": true,
+                    "topLevel": false
+                }
+            },
+            "comments": {},
+            "currentCostume": 1,
+            "costumes": [{
+                "assetId": "27efd313c3b05eb3e3498bd20da809d9",
+                "name": "two",
+                "bitmapResolution": 2,
+                "md5ext": "27efd313c3b05eb3e3498bd20da809d9.png",
+                "dataFormat": "png",
+                "rotationCenterX": 449.5,
+                "rotationCenterY": 360
+            }, {
+                "assetId": "b7b57428f411bfec74b540425bf21dbf",
+                "name": "back",
+                "bitmapResolution": 2,
+                "md5ext": "b7b57428f411bfec74b540425bf21dbf.png",
+                "dataFormat": "png",
+                "rotationCenterX": 360,
+                "rotationCenterY": 360
+            }, {
+                "assetId": "b6c854d67fbfdc4f93ce5165c8f1fd97",
+                "name": "tiles_shoaib",
+                "bitmapResolution": 2,
+                "md5ext": "b6c854d67fbfdc4f93ce5165c8f1fd97.png",
+                "dataFormat": "png",
+                "rotationCenterX": 449.5,
+                "rotationCenterY": 360
+            }],
+            "sounds": [{
+                "assetId": "83a9787d4cb6f3b7632b4ddfebf74367",
+                "name": "pop",
+                "dataFormat": "wav",
+                "format": "",
+                "rate": 48000,
+                "sampleCount": 1123,
+                "md5ext": "83a9787d4cb6f3b7632b4ddfebf74367.wav"
+            }],
+            "volume": 100,
+            "layerOrder": 0,
+            "tempo": 60,
+            "videoTransparency": 50,
+            "videoState": "on",
+            "textToSpeechLanguage": null
+        }, {
+            "isStage": false,
+            "name": "chota bheem-11",
+            "variables": {},
+            "lists": {},
+            "broadcasts": {},
+            "blocks": {
+                "RfQiYmw0ay|/jbF:n]?G": {
+                    "opcode": "event_whenbroadcastreceived",
+                    "next": "1MDZa.(_8qmw=*Q3V.MS",
+                    "parent": null,
+                    "inputs": {},
+                    "fields": {
+                        "BROADCAST_OPTION": ["Done", "0bXua}n4vCATlxO^M2Ml"]
+                    },
+                    "shadow": false,
+                    "topLevel": true,
+                    "x": 40,
+                    "y": 181
+                },
+                "1MDZa.(_8qmw=*Q3V.MS": {
+                    "opcode": "motion_gotoxy",
+                    "next": null,
+                    "parent": "RfQiYmw0ay|/jbF:n]?G",
+                    "inputs": {
+                        "X": [1, [4, "-39"]],
+                        "Y": [1, [4, "-68"]]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "pcnA7#l-C^QB!Y;T?{qY": {
+                    "opcode": "event_whenflagclicked",
+                    "next": null,
+                    "parent": null,
+                    "inputs": {},
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": true,
+                    "x": 684,
+                    "y": 128
+                }
+            },
+            "comments": {},
+            "currentCostume": 0,
+            "costumes": [{
+                "assetId": "f24cdac9007587527f2e7c31b7e463b9",
+                "name": "chota bheem-11",
+                "bitmapResolution": 2,
+                "md5ext": "f24cdac9007587527f2e7c31b7e463b9.png",
+                "dataFormat": "png",
+                "rotationCenterX": 360,
+                "rotationCenterY": 360
+            }],
+            "sounds": [],
+            "volume": 100,
+            "layerOrder": 1,
+            "visible": true,
+            "x": -19,
+            "y": -68,
+            "size": 100,
+            "direction": 90,
+            "draggable": false,
+            "rotationStyle": "all around"
+        }, {
+            "isStage": false,
+            "name": "three",
+            "variables": {},
+            "lists": {},
+            "broadcasts": {},
+            "blocks": {
+                "MYu?{*.=AX{#nVxeN08_": {
+                    "opcode": "event_whenflagclicked",
+                    "next": "i--6Ci5Sh9ZQx`nsG1rr",
+                    "parent": null,
+                    "inputs": {},
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": true,
+                    "x": 623,
+                    "y": 75
+                },
+                "wK9G`!wxkYwl5o.UHGna": {
+                    "opcode": "procedures_definition",
+                    "next": "BZ`M3w@p|4K[j.xAKTAl",
+                    "parent": null,
+                    "inputs": {
+                        "custom_block": [1, "aEnK:7S~iOIk`@r@nGZ]"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": true,
+                    "x": 44,
+                    "y": 44
+                },
+                "aEnK:7S~iOIk`@r@nGZ]": {
+                    "opcode": "procedures_prototype",
+                    "next": null,
+                    "parent": "wK9G`!wxkYwl5o.UHGna",
+                    "inputs": {},
+                    "fields": {},
+                    "shadow": true,
+                    "topLevel": false,
+                    "mutation": {
+                        "tagName": "mutation",
+                        "children": [],
+                        "proccode": "Check",
+                        "argumentids": "[]",
+                        "argumentnames": "[]",
+                        "argumentdefaults": "[]",
+                        "warp": "false"
+                    }
+                },
+                "BZ`M3w@p|4K[j.xAKTAl": {
+                    "opcode": "control_forever",
+                    "next": null,
+                    "parent": "wK9G`!wxkYwl5o.UHGna",
+                    "inputs": {
+                        "SUBSTACK": [2, "ZHj?n4S..oFf7lR(K@Xb"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "ZHj?n4S..oFf7lR(K@Xb": {
+                    "opcode": "control_if",
+                    "next": null,
+                    "parent": "BZ`M3w@p|4K[j.xAKTAl",
+                    "inputs": {
+                        "CONDITION": [2, "Gy,fNla.mBnKtc]WR}+~"],
+                        "SUBSTACK": [2, "/*]pM%`Rdk~FP/2MU,ka"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "i--6Ci5Sh9ZQx`nsG1rr": {
+                    "opcode": "procedures_call",
+                    "next": null,
+                    "parent": "MYu?{*.=AX{#nVxeN08_",
+                    "inputs": {},
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false,
+                    "mutation": {
+                        "tagName": "mutation",
+                        "children": [],
+                        "proccode": "Check",
+                        "argumentids": "[]",
+                        "warp": "false"
+                    }
+                },
+                "Gy,fNla.mBnKtc]WR}+~": {
+                    "opcode": "sensing_touchingobject",
+                    "next": null,
+                    "parent": "ZHj?n4S..oFf7lR(K@Xb",
+                    "inputs": {
+                        "TOUCHINGOBJECTMENU": [1, "}5W0/a~%@w:A^n:+S,HG"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "}5W0/a~%@w:A^n:+S,HG": {
+                    "opcode": "sensing_touchingobjectmenu",
+                    "next": null,
+                    "parent": "Gy,fNla.mBnKtc]WR}+~",
+                    "inputs": {},
+                    "fields": {
+                        "TOUCHINGOBJECTMENU": ["chota bheem-11", null]
+                    },
+                    "shadow": true,
+                    "topLevel": false
+                },
+                "/*]pM%`Rdk~FP/2MU,ka": {
+                    "opcode": "looks_switchbackdropto",
+                    "next": "Z;uIG=E5jvl{d7i4ec)`",
+                    "parent": "ZHj?n4S..oFf7lR(K@Xb",
+                    "inputs": {
+                        "BACKDROP": [1, "YlP0za)x__l:;C04d|Cg"]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                },
+                "YlP0za)x__l:;C04d|Cg": {
+                    "opcode": "looks_backdrops",
+                    "next": null,
+                    "parent": "/*]pM%`Rdk~FP/2MU,ka",
+                    "inputs": {},
+                    "fields": {
+                        "BACKDROP": ["tiles_shoaib", null]
+                    },
+                    "shadow": true,
+                    "topLevel": false
+                },
+                "Z;uIG=E5jvl{d7i4ec)`": {
+                    "opcode": "event_broadcast",
+                    "next": null,
+                    "parent": "/*]pM%`Rdk~FP/2MU,ka",
+                    "inputs": {
+                        "BROADCAST_INPUT": [1, [11, "Done", "0bXua}n4vCATlxO^M2Ml"]]
+                    },
+                    "fields": {},
+                    "shadow": false,
+                    "topLevel": false
+                }
+            },
+            "comments": {},
+            "currentCostume": 0,
+            "costumes": [{
+                "assetId": "174e3330f4abf784cbf6035e15f17f70",
+                "name": "three",
+                "bitmapResolution": 2,
+                "md5ext": "174e3330f4abf784cbf6035e15f17f70.png",
+                "dataFormat": "png",
+                "rotationCenterX": 84,
+                "rotationCenterY": 143
+            }],
+            "sounds": [],
+            "volume": 100,
+            "layerOrder": 2,
+            "visible": true,
+            "x": -1.7763568394002505e-14,
+            "y": -98,
+            "size": 65,
+            "direction": 90,
+            "draggable": false,
+            "rotationStyle": "all around"
+        }],
+        "monitors": [],
+        "extensions": [],
+        "meta": {
+            "semver": "3.0.0",
+            "vm": "0.2.0-prerelease.20190822194548",
+            "agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) ScratchDesktop/3.6.0 Chrome/69.0.3497.128 Electron/4.2.9 Safari/537.36"
+        }
+    });
+};
+
+
+export default projectData;
