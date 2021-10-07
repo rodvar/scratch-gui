ce62f8175ba930ee524917108c07b070d35a7b8d
Rodrigo Varela <rodvar@gmail.com>
2021-10-07 17:00:34 +1100

    Remove unused code

--- src/components/stage-wrapper/stage-wrapper.jsx
+++ src/components/stage-wrapper/stage-wrapper.jsx
@@ -22,39 +22,6 @@ const StageWrapperComponent = function (props) {
         vm
     } = props;
 
-    // let pos1, pos2, pos3, pos4;
-
-    // const elementDrag = (e) => {
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     // calculate the new cursor position:
-    //     pos1 = pos3 - e.clientX;
-    //     pos2 = pos4 - e.clientY;
-    //     pos3 = e.clientX;
-    //     pos4 = e.clientY;
-    //     // set the element's new position:
-    //     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    //     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    //   }
-    
-    // const closeDragElement = () => {
-    //     // stop moving when mouse button is released:
-    //     document.onmouseup = null;
-    //     document.onmousemove = null;
-    // }
-
-    // const handleMouseDown = (e) => { 
-    //     console.log(`Handling mouse down ${JSON.stringify(e)}`);
-    //     e = e || window.event;
-    //     e.preventDefault();
-    //     // get the mouse cursor position at startup:
-    //     pos3 = e.clientX;
-    //     pos4 = e.clientY;
-    //     document.onmouseup = closeDragElement;
-    //     // call a function whenever the cursor moves:
-    //     document.onmousemove = elementDrag;
-    // };
-
     const content = <Box
         className={classNames(
             styles.stageWrapper,
@@ -91,9 +58,6 @@ const StageWrapperComponent = function (props) {
     </Box>
 
     return (
-        // <div onClick={(e) => {handleMouseDown(e);}}
-        //     onMouseDown={(e) => {handleMouseDown(e);}}
-        //     >
         <Draggable
             initialPos={{x: 100, y: 200}}
             className="my-draggable"
